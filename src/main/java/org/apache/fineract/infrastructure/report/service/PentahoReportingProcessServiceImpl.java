21fb4b79777e42ba4de807ff8fa5e70851d550ff
wairuru <peter@lendable.io>
2025-09-27 11:54:21 +0300

    remove the error

--- src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
+++ src/main/java/org/apache/fineract/infrastructure/report/service/PentahoReportingProcessServiceImpl.java
@@ -180,7 +180,7 @@ public class PentahoReportingProcessServiceImpl implements ReportingProcessServi
             
             // Fallback: shared version
             if (!new File(reportPath).exists()) {
-                String sharedPath = getSharedReportsPath() + reportName + ".prpt";f
+                String sharedPath = getSharedReportsPath() + reportName + ".prpt";
                 if (new File(sharedPath).exists()) {
                     reportPath = sharedPath;
                 }
